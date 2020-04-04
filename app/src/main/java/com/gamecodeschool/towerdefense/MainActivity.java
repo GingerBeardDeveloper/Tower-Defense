93c258c55aa8f79b186638a698ab658092e3c2a5
Jimmy Le <jimmyle1997@gmail.com>
2020-04-03 19:51:45 -0700

    Added TowerDefenseGame.java and an Activity.java for the game

--- app/src/main/java/com/gamecodeschool/towerdefense/MainActivity.java
+++ app/src/main/java/com/gamecodeschool/towerdefense/MainActivity.java
@@ -9,6 +9,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Get the pixel dimensions of the screen
+        Display display = getWindowManager().getDefaultDisplay();
+
+        // Initialize the result into a Point object
+        Point size = new Point();
+        display.getSize(size);
+
+        // Create a new instance of the TowerGame class
+        mTowerGame = new TowerGame(this, size);
+
+        // Make the towerDefense the view of the Activity
         setContentView(R.layout.activity_main);
     }
 }
