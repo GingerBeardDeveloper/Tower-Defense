189580b4a57e785a236b3e34de13b2d690b88544
Chris West <chriswest4@gmail.com>
2020-04-06 18:19:25 -0700

    Added waves to HardMap

--- app/src/main/java/com/gamecodeschool/towerdefense/HardMap.java
+++ app/src/main/java/com/gamecodeschool/towerdefense/HardMap.java
@@ -5,8 +5,11 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 
+import java.util.ArrayList;
+
 public class HardMap extends Map {
 
+    ArrayList<Wave> alienWaves;
     Paint pathPaint;
 
     public HardMap(Canvas mCanvas) {
@@ -14,10 +17,17 @@ public class HardMap extends Map {
         //end = new Point((int) (mCanvas.getWidth() * 0.2),mCanvas.getHeight() / 2);
         pathPaint = new Paint();
         pathPaint.setColor(Color.GRAY);
+        alienWaves = new ArrayList<Wave>();
     }
 
     public void draw(Canvas mCanvas) {
         mCanvas.drawRect(0, (float) (mCanvas.getHeight() / 2 + 40), (float) (mCanvas.getWidth() * 0.8), (float) (mCanvas.getHeight() / 2 - 40), pathPaint);
 
+    }
+
+    private void setupWaves(int numWaves) {
+        for (int i = 0; i < numWaves; i++) {
+            alienWaves.add(new Wave(5, 2));
+        }
     }
 }
